777ce228a62a204c90899950564b603809a3e05e
Barry-9988 <1003351513@qq.com>
2020-07-24 22:54:19 +0800

    添加项目文件。

---
+++ 测试上传/测试2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	int i = 1;
+	int s = 0;
+
+	while (i <= 100) {
+		s += i;
+		i++;
+	}
+
+	cout << "s=" << s << endl;
+
+	system("pause");
+	return 0;
+}
